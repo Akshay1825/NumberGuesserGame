985aba886350c54b0e8ed50428f4dc54805d1b8d
Akshay Utekar <akshayutekar1825@gmail.com>
2024-09-26 00:42:24 +0530

    Add project files.

---
+++ NumberGuesserDemo/Model/NumberGuesserGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesserDemo.Model
+{
+    internal class NumberGuesserGame
+    {
+        int numberToGuess;
+        int attempts;
+
+        public NumberGuesserGame()
+        {
+            Random random = new Random();
+            numberToGuess = random.Next(1, 101);
+            attempts = 0;
+        }
+
+        public void StartGame()
+        {
+            bool hasGuessedCorrectly = false;
+
+            while (!hasGuessedCorrectly)
+            {
+                Console.Write("Enter a guess number between 1 to 100: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int guess))
+                {
+                    attempts++;
+                    hasGuessedCorrectly = CheckGuess(guess);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please enter a number between 1 and 100");
+                }
+            }
+
+            Console.WriteLine($"You took {attempts} tries! You win the game!");
+        }
+
+        private bool CheckGuess(int guess)
+        {
+            string message = guess < numberToGuess ? "Sorry,Too Low" :
+                             guess > numberToGuess ? "Sorry,Too High" :
+                             "Yayy! Your guessed number is correct";
+
+            Console.WriteLine(message);
+
+            return guess == numberToGuess;
+        }
+    }
+}
---
+++ NumberGuesserDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using NumberGuesserDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesserDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Guess Number Game!");
+            Console.WriteLine("You will be asked to guess a number to win the game.");
+
+            bool play = true;
+
+            while (play)
+            {
+                NumberGuesserGame numberGuesserGame = new NumberGuesserGame();
+                numberGuesserGame.StartGame();
+
+                Console.Write("Do you want to play again? (yes/no): ");
+                string playAgainInput = Console.ReadLine().ToLower();
+
+                play = playAgainInput == "yes";
+                if (!play)
+                {
+                    Console.WriteLine("Thanks for playing! Goodbye.");
+                }
+            }
+        }
+    }
+}
